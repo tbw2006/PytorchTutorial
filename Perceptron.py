ab7ced985147956b3df01b5de40a3bd968e499b5
tbw <2725989503@qq.com>
2025-04-09 14:41:58 +0800

    modify to improve accuracy,create traim_utils to load functions 2025/04/09

--- Perceptron.py
+++ Perceptron.py
@@ -1,9 +1,12 @@
 import torch
+import math
 from torch import nn
 from d2l import torch as d2l
 
-from SoftmaxRegression import cross_entropy, get_data_fashion_mnist,sgd, train
-
+from train_utils import train,sgd,get_data_fashion_mnist,cross_entropy
+import importlib
+import train_utils
+importlib.reload(train_utils)
 batch_size = 256
 train_iter , test_iter = get_data_fashion_mnist(batch_size=batch_size)
 
@@ -11,10 +14,16 @@ num_inupts = 28*28
 num_hiddens = 256
 num_outputs = 10
 
-w1 = torch.randn(num_inupts,num_hiddens,requires_grad=True)#换成0矩阵会怎么样
-b1 = torch.randn(num_hiddens,requires_grad=True)
+'''w1 = torch.randn(num_inupts,num_hiddens,requires_grad=True)#换成0矩阵会怎么样
+b1 = torch.zeros(num_hiddens,requires_grad=True)
 w2 = torch.randn(num_hiddens,num_outputs,requires_grad=True)
-b2 = torch.randn(num_outputs,requires_grad=True)
+b2 = torch.zeros(num_outputs,requires_grad=True)'''
+b1 = torch.zeros(num_hiddens,requires_grad=True)
+b2 = torch.zeros(num_outputs,requires_grad=True)
+w1 = torch.randn(num_inupts, num_hiddens, requires_grad=True) 
+w1.data *=  math.sqrt(2.0 / num_inupts)
+w2 = torch.randn(num_hiddens, num_outputs, requires_grad=True) 
+w2.data *=  math.sqrt(2.0 / num_hiddens)
 params = [w1,b1,w2,b2]
 
 num_epochs = 10
@@ -22,17 +31,32 @@ lr = 0.1
 
 def ReLU(x):
     a = torch.zeros_like(x)
-    return max(a,x)
+    return torch.max(a,x)
 
-criterion = cross_entropy
-updater = sgd(params = params,lr=lr)
+#criterion = cross_entropy
+#updater = sgd(params = params,lr=lr)#换成官方的试试
+updater = torch.optim.SGD(params=params,lr=lr)
+criterion = nn.CrossEntropyLoss()
+class MLP:
+    def __init__(self,w1,b1,w2,b2,activation):
+        self.w1 = w1
+        self.b1 = b1
+        self.w2 = w2
+        self.b2 = b2
+        self.activation = activation
 
-def net(x,w1,b1,w2,b2,activation=ReLU):
-    outputs = torch.matmul(w1,x)+b1
-    outputs = activation(outputs)
-    outputs = torch.matmul(w2,outputs)+b2
-    return outputs
+    def __call__(self, x):
+        #print("call")
+        x = x.reshape(-1,num_inupts)
+        h = torch.matmul(x,self.w1) +self.b1
+        h = self.activation(h)
+        return torch.matmul(h,self.w2)+self.b2
 
-train(net = net,updater=updater,train_iter=train_iter,
-test_iter = test_iter,num_epochs=num_epochs)
+net = MLP(w1,b1,w2,b2,ReLU)
+train(net = net,train_iter=train_iter,
+test_iter = test_iter,updater=updater,
+num_epochs=num_epochs,params=params,criterion=criterion)
+
+
+
 
--- train_utils.py
+++ train_utils.py
@@ -4,7 +4,6 @@ from torchvision import transforms
 from torch.utils import data
 from d2l import torch as d2l
 
-
 def get_data_fashion_mnist(batch_size = 32,resize = None):
 
     trans = transforms.Compose([transforms.ToTensor()])
@@ -27,25 +26,15 @@ def get_data_fashion_mnist(batch_size = 32,resize = None):
                                 shuffle=True),
             data.DataLoader(dataset=test_set,batch_size=batch_size,
                                 shuffle=False)       )
-    timer = d2l.Timer()                 #这里可以看读取速度
+    '''timer = d2l.Timer()                 #这里可以看读取速度
     for x,y in train_iter:
         continue
-    print(f'{timer.stop():.2f} sec')     
-
-
-
-batch_size = 32
-train_iter ,test_iter = get_data_fashion_mnist(batch_size)
-
-#初始化
-input_size = 784
-output_size = 10
-w = torch.normal(0,0.01,size = (input_size,output_size),requires_grad=True)
-b = torch.zeros(output_size,requires_grad=True)
+    print(f'{timer.stop():.2f} sec')     '''
 
 
 def softmax(x):
-    exp_x = torch.exp(x)
+    x_max = x.max(dim=1,keepdim = True)[0]
+    exp_x = torch.exp(x-x_max)
     partition = exp_x.sum(dim=1,keepdim=True)
     return exp_x / partition
 #测试一下
@@ -55,16 +44,16 @@ print(x)
 print(x.sum(1))'''
 
 
-def net(x):
-    return softmax(torch.matmul(x.reshape(-1,w.shape[0]),w)+b)
-
-def cross_entropy(y_hat,y):
-    return -1 * torch.log(y_hat[range(len(y_hat)),y])
-
 def accuracy(y_hat,y):
     predict = torch.argmax(y_hat,dim = 1)
     correct = (y==predict).sum().item()
     return correct 
+
+
+def cross_entropy(y_hat,y):
+    return -1 * torch.log(y_hat[range(len(y_hat)),y] +1e-10)
+#   +1e-10防止log0的出现-> 负无穷
+
 
 def evaluate_accuracy(net,data_iter):
     if isinstance(net,torch.nn.Module):
@@ -79,6 +68,8 @@ def evaluate_accuracy(net,data_iter):
     return correct/total
 
 #print(evaluate_accuracy(net,test_iter))测试
+
+
 def train_epoch(net,updater,criterion,train_iter,params):
     if isinstance(net,torch.nn.Module):
         net.train()
@@ -89,34 +80,41 @@ def train_epoch(net,updater,criterion,train_iter,params):
         if isinstance(updater,torch.optim.Optimizer):
             updater.zero_grad()
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(params,max_norm=10.0)
             updater.step()
             metric.add(loss.sum().item(),accuracy(y_hat,y),len(y))
 
         else:
             loss.sum().backward()#只有标量才能反向传播
-            updater(params)#what in ()
+            updater(params)
             metric.add(loss.sum().item(),accuracy(y_hat,y),len(y))
 
     return metric[0]/metric[2] , metric[1]/ metric[2]
 
 
-def sgd(params,lr = 0.03,batch_size = 32):
-    with torch.no_grad():
-        for param in params:
-            if param.grad is not None:
-                param -= lr *  param.grad / batch_size
-                param.grad.zero_()
 
-net = net
-updater = sgd
-num_epochs = 10
-lr = 0.03
-criterion = cross_entropy
-def train(net,updater,train_iter,test_iter,num_epochs,params):
+def sgd(params,lr = 0.03,batch_size = 32):
+    def updater(_):
+        with torch.no_grad():
+            for param in params:
+                if param.grad is not None:
+                    
+                    param -= lr *  param.grad / batch_size
+                    param.grad.zero_()
+    return updater
+
+
+
+
+def train(net,train_iter,test_iter,
+updater=None,num_epochs=10,params=None,lr = 0.03,criterion=cross_entropy):
+    if updater is None and params is not None:
+        updater = sgd(params=params,lr=lr)
     for epoch in range(num_epochs):
         loss , train_accuracy = train_epoch(net,updater,criterion,train_iter,params)
         test_accuracy = evaluate_accuracy(net,test_iter)
+        if params is not None:
+            print(f"w1范围: min={params[0].min().item():.4f}, max={params[0].max().item():.4f}")
+            print(f"w2范围: min={params[2].min().item():.4f}, max={params[2].max().item():.4f}")
         print(f'epoch: {epoch+1} , loss: {loss:.4f}' )
         print(f'train_accuracy: {train_accuracy:.4f} , test_accuracy: {test_accuracy}')
-
-train(net,updater,train_iter,test_iter,num_epochs,[w,b])
