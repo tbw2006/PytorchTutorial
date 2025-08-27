46f1ea77ab9633e5c3419e8f5be664e030f5636d
tbw <2725989503@qq.com>
2025-08-27 23:59:22 +0800

    2025/08/27

---
+++ EncoderDecoder.py
@@ -0,0 +1,34 @@
+from torch import nn
+
+#
+
+class Encoder(nn.Module):
+    def __init__(self, **kwargs):
+        super(Encoder, self).__init__(**kwargs)
+
+    def forward(self, X, *args):
+        raise NotImplementedError
+
+#
+
+class Decoder(nn.Module):
+    def __init__(self, **kwargs):
+        super(Decoder, self).__init__(**kwargs)
+
+    def init_state(self, enc_outputs, *args):
+        raise NotImplementedError
+
+    def forward(self, X, state):
+        raise NotImplementedError
+
+
+#
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(EncoderDecoder, self).__init__(**kwargs)
+    
+    def forward(self, enc_x, dec_x, *args):
+        enc_outputs =  self.encoder(enc_x)
+        dec_state = self.decoder.init_state(enc_outputs, *args)
+        return self.decoder(dec_x, dec_state)
